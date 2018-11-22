62f1db1916b45e5eccc63d510ba756ef43ae3a89
Oscar Negrete <oscar.negrete09@gmail.com>
2018-11-22 17:05:06 -0500

    Initial commit

---
+++ DemoLibrary/PhysicalProductModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DemoLibrary {
+  public class PhysicalProductModel {
+    public string Title { get; set; }
+    public bool HasOrderBeenCompleted { get; private set; }
+
+    public void ShipItem (CustomerModel customer) {
+      if (HasOrderBeenCompleted == false) {
+        Console.WriteLine ($"Simulating shipping { Title } to { customer.FirstName } in { customer.City }");
+        HasOrderBeenCompleted = true;
+      }
+    }
+
+  }
+}
